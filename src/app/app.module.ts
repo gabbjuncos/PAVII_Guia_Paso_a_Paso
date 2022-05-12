056ce64591089ead1bbd3b9aa442445b02ba1088
gabbjuncos <gabbjuncos@gmail.com>
2022-05-12 17:02:59 -0300

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { ArticulosFamilia } from './components/articulos-familia/articulos-familia.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, InicioComponent],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    InicioComponent,
+    ArticulosFamilia,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
